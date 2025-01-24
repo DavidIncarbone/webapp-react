7381ef0588527a9289c8a61ac4ca91731ff17f0c
DavidIncarbone <david.incarbone@gmail.com>
2025-01-24 17:35:26 +0100

    Add navbar and api's for films

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+import Navbar from "./Navbar";
+
+export default function Header() {
+    return (
+
+        <header className="bg-secondary-subtle">
+            <Navbar />
+        </header>
+
+    )
+}
---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,5 @@
+export default function movieDeatails() {
+    return (
+        <h1>Details</h1>
+    )
+}
